941b448aa91623b1b0b33b82d012b1a1a30649f8
HungDaiTruong <hungdaitruong@gmail.com>
2023-04-25 02:43:02 +0200

    GY-RACE_patch3_Menu
    25/04/2023

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    public GameObject vehicle;
+    public GameObject collectionObject;
+    public GameObject lastCheckpoint;
+    public bool trigger = false;
+    static public int lap = 0;
+    static public int count = 0;
+    static private int checkpointNumber;
+    private bool lapped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectionObject = transform.parent.gameObject;
+        foreach (Transform g in transform.GetComponentsInChildren<Transform>())
+        {
+            g.GetComponent<BoxCollider>().enabled = false;
+            checkpointNumber++;
+        }
+        collectionObject.transform.GetChild(0).GetComponent<BoxCollider>().enabled = true;
+        lastCheckpoint = collectionObject.transform.GetChild(0).gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(count == checkpointNumber)
+        {
+            count = 0;
+            lastCheckpoint.GetComponent<BoxCollider>().enabled = true;
+            lapped = true;
+            trigger = false;
+        }
+
+        if(count == 1 && lapped)
+        {
+            lap++;
+            lapped = false;
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        if(collider.gameObject == vehicle && transform.parent != null)
+        {
+            count++;
+            trigger = true;
+            collectionObject.transform.GetChild(count-1).GetComponent<BoxCollider>().enabled = false;
+            if (count < checkpointNumber)
+            {
+                collectionObject.transform.GetChild(count).GetComponent<BoxCollider>().enabled = true;
+            }
+        }
+        Debug.Log("count : " + count + "lap : " + lap);
+    }
+}
