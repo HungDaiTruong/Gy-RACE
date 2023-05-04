08b530dc3e3ee8f743a5b6d0e7f36bc230891b66
ruth <45215676+RoutyS@users.noreply.github.com>
2023-05-04 12:37:12 +0200

    Usernames Patch
    04/05/2023

--- Assets/Scripts/OutOfBounds.cs
+++ Assets/Scripts/OutOfBounds.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-<<<<<<< Updated upstream
 using UnityEngine.SceneManagement;
 
 public class OutOfBounds : MonoBehaviour
@@ -12,27 +11,11 @@ public class OutOfBounds : MonoBehaviour
     void Start()
     {
         checkpointScript = FindObjectOfType<CheckpointScript>();
-=======
-
-public class OutOfBounds : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
->>>>>>> Stashed changes
     }
 
     public void OnTriggerEnter(Collider collider)
     {
         Debug.Log("Out of Bounds");
-<<<<<<< Updated upstream
 
         // If a player reaches the out of bound area, it respawns according to its latest checkpoint's position and rotation
         if (collider.GetComponent<PlayerLapper>())
@@ -42,9 +25,5 @@ public class OutOfBounds : MonoBehaviour
             collider.transform.position = checkpointScript.collectionObject.transform.GetChild(playerLapper.checkpointIndex).transform.position;
             collider.transform.rotation = checkpointScript.collectionObject.transform.GetChild(playerLapper.checkpointIndex).transform.rotation * Quaternion.Euler(0, -90, 0);
         }
-=======
-        UnityEditor.EditorApplication.isPlaying = false;
-        Application.Quit();
->>>>>>> Stashed changes
     }
 }
--- Assets/Scripts/RaceTimer.cs
+++ Assets/Scripts/RaceTimer.cs
@@ -2,18 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-<<<<<<< Updated upstream
 using UnityEngine.SceneManagement;
 using TMPro;
-=======
->>>>>>> Stashed changes
 
 public class RaceTimer : MonoBehaviour
 {
     public float time;
     public bool timerOn = false;
     public Text timerText;
-<<<<<<< Updated upstream
     public TMP_Text scoreText;
     public GameObject scoreboard;
 
@@ -21,13 +17,10 @@ public class RaceTimer : MonoBehaviour
 
     private GameObject player1;
     private GameObject player2;
-=======
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< Updated upstream
         scoreboard.SetActive(false);
         timerOn = true;
         timerText.fontSize = 16;
@@ -36,20 +29,13 @@ public class RaceTimer : MonoBehaviour
 
         player1 = GameObject.FindGameObjectWithTag("Player");
         player2 = GameObject.FindGameObjectWithTag("Player2");
-=======
-        timerOn = true;
->>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         // Ends when the 4th lap is starting, for a total of 3 laps
         if(timerOn && !(playerLapper.lap == 4 && playerLapper.checkpointIndex == 0))
-=======
-        if(timerOn && !(CheckpointScript.lap == 3 && CheckpointScript.count == 1))
->>>>>>> Stashed changes
         {
             time += Time.deltaTime;
             UpdateTimer(time);
@@ -58,37 +44,25 @@ public class RaceTimer : MonoBehaviour
         {
             Debug.Log(time);
             Debug.Log("Race is Finished");
-<<<<<<< Updated upstream
-=======
-            time = 0;
->>>>>>> Stashed changes
             GameIsDone();
         }
     }
 
     void UpdateTimer(float currentTime)
     {
-<<<<<<< Updated upstream
         // Updates the timer and displays the time, laps and checkpoints counts
-=======
->>>>>>> Stashed changes
         currentTime += 1;
 
         float minutes = Mathf.FloorToInt(currentTime / 60);
         float seconds = Mathf.FloorToInt(currentTime % 60);
 
-<<<<<<< Updated upstream
         timerText.text = string.Format(" {0:00}  :  {1:00}\n Lap {2}/3\n Checkpoint {3}/{4}", minutes, seconds, playerLapper.lap, playerLapper.checkpointIndex + 1, CheckpointScript.checkpointNumber);
         // Value used as a display during the scoreboard
         scoreText.text = string.Format("{0:00}  :  {1:00}", minutes, seconds);
-=======
-        timerText.text = string.Format("{0:00}  :  {1:00}", minutes, seconds);
->>>>>>> Stashed changes
     }
 
     public void GameIsDone()
     {
-<<<<<<< Updated upstream
         // Stops the game once the laps are completed
         scoreboard.SetActive(true);
         playerLapper.checkpointIndex = 0;
@@ -104,9 +78,5 @@ public class RaceTimer : MonoBehaviour
         playerLapper.lap = 1;
         Time.timeScale = 1;
         SceneManager.LoadScene("MenuScene");
-=======
-        UnityEditor.EditorApplication.isPlaying = false;
-        Application.Quit();
->>>>>>> Stashed changes
     }
 }
